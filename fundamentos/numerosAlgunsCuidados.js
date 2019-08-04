0b7bda8aadabb63e1f5341b91fb3a1ac0306c536
Fabricio Santos <fabriciobs2000@gmail.com>
2019-08-04 20:59:30 -0300

    trabalhando com a biblioteca Math, alguns cuidados com numeros e sobre Strings

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,10 @@
+console.log(7 / 0)
+//podemos fazer operações matematicas com strings
+console.log("10" / 2)
+
+console.log('3' + 2) //string ganha 
+console.log('3' - 2)
+
+console.log("show!" * 2)
+console.log(0.1 + 0.7) //falta de precisao com numeros flutuantes
+console.log((10.354).toFixed(2))
